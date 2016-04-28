412facaa138670a8b7df56d704aae19570aa88a4
Sebastian Green-Husted <ricecake@tfm.nu>
2016-04-28 15:53:13 -0400

    add static files

---
+++ apps/wnkd_web/priv/static/js/common.js
@@ -0,0 +1,10 @@
+requirejs.config({
+	baseUrl: 'static/js',
+	paths: {
+		jquery: 'lib/ext/jquery-2.1.1.min',
+		underscore: 'lib/ext/underscore-min',
+		text: 'lib/ext/text',
+		tpl: 'lib/ext/tpl',
+		template: '../template'
+	}
+});
