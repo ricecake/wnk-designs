f25edae0e2af3ba90009b2e77685a313b1f64954
Sebastian Green-Husted <ricecake@tfm.nu>
2016-05-29 20:53:21 -0400

    wip pull images from s3

--- apps/wnkd_web/src/wnkd_web_index.erl
+++ apps/wnkd_web/src/wnkd_web_index.erl
@@ -4,8 +4,15 @@
 
 init(Req, Args) ->
 	{ok, Items} = wnkd_item:list_active(),
-	{ok, Body} = wnkd_web_index_dtl:render([{page, <<"index">>}, {items, Items}]),
+	PartitionedItems = category_partition(Items, #{}),
+	io:format("~p~n", [PartitionedItems]),
+	{ok, Body} = wnkd_web_index_dtl:render([{page, <<"index">>} |maps:to_list(PartitionedItems)]),
 	Req2 = cowboy_req:reply(200, [
 		{<<"content-type">>, <<"text/html">>}
 	], Body, Req),
 	{ok, Req2, Args}.
+
+category_partition([], Acc) -> Acc;
+category_partition([#{ <<"category">> := Category } = Item | Rest], Acc) ->
+	ThisCategory = maps:get(Category, Acc, []),
+	category_partition(Rest, Acc#{ Category => [Item |ThisCategory]}).
